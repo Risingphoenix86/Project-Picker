c7e92f8fc10791730491ea8fbabc00123ba64e56
Kyle Lux <kyleslux86@gmail.com>
2021-11-02 18:50:06 -0400

    fixed route import paths

--- controllers/apis/projectRoutes.js
+++ controllers/apis/projectRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Project } = require('../../models/project');
 
 
 
--- controllers/apis/userRoutes.js
+++ controllers/apis/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../../models/user');
 
 router.post('/login', async (req, res) => {
     try {
