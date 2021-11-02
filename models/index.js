41a526488ac8b96cd0cbf293eb50f36cf50eabb3
Kyle Lux <kyleslux86@gmail.com>
2021-11-02 19:48:47 -0400

    more tests

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const User = require('./User');
-const Project = require('./Project');
+const User = require('./User.js');
+const Project = require('./Project.js');
 
 User.hasMany(Project, {
     foreignKey: 'user_id',
