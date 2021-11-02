ff891b24399b4ad10da62fa05917a90fcb5956cd
Kyle Lux <kyleslux86@gmail.com>
2021-11-02 19:17:46 -0400

    more testing

--- controllers/apis/userRoutes.js
+++ controllers/apis/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models/user');
+const { User } = require('../../models/User');
 
 router.post('/login', async (req, res) => {
     try {
