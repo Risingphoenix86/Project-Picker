8cb4a2fa2ac16c35bf03e30e26ad90fd1ca5e48e
Kyle Lux <kyleslux86@gmail.com>
2021-11-02 21:22:55 -0400

    merging branches for fresh start next session

--- controllers/apis/userRoutes.js
+++ controllers/apis/userRoutes.js
@@ -1,6 +1,28 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//Create user
+router.post("/", (req, res) => {
+    User.create({
+      username: req.body.username,
+      password: req.body.password,
+      email: res.body.email
+    })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.userId = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+  
 router.post('/login', async (req, res) => {
     try {
         const userData = await User.findOne({ where: { email: req.body.email } });
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,23 +2,23 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', withAuth, async (req,res) => {
-//     try {
-//         const userData = await User.findAll({
-//             attributes: { exclude: ['password'] },
-//             order: [['name', 'ASC']],
-//         });
+router.get('/', withAuth, async (req,res) => {
+    try {
+        const userData = await User.findAll({
+            attributes: { exclude: ['password'] },
+            order: [['username', 'ASC']],
+        });
 
-//         const users = iserData.map((project) => project.get({ plain: true}));
+        const users = userData.map((project) => project.get({ plain: true}));
 
-//         res.render('homepage', {
-//             users,
-//             logged_in: req.session.logged_in    
-//         });
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+        res.render('homepage', {
+            users,
+            logged_in: req.session.logged_in    
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
