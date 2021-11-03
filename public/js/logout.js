97c39bf7d9f0b70184f868e1b73c794e81163ff1
morganmmoore <morganmmoore0@gmail.com>
2021-11-02 20:45:50 -0400

    added signup

--- public/js/logout.js
+++ public/js/logout.js
@@ -2,7 +2,7 @@ const logout = async () => {
     const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-    });
+    })
   
     if (response.ok) {
       document.location.replace('/login');
