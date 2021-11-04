3fc80fac56bbf0864403d4090da174823a132471
morganmmoore <morganmmoore0@gmail.com>
2021-11-04 18:42:58 -0400

    fixed small error

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 //Create user
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
     User.create({
       username: req.body.username,
       password: req.body.password,
