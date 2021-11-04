a272ad8d7db8dc27926d9b6384a4e30623891610
morganmmoore <morganmmoore0@gmail.com>
2021-11-04 19:01:15 -0400

    Fixed signup

--- public/js/login.js
+++ public/js/login.js
@@ -22,12 +22,12 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#name-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
     if (username && email && password) {
-      const response = await fetch('/api/users', {
+      const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
