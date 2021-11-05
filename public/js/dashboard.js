6e5009c2da713fc730397bdb623d3a09895c74b2
morganmmoore <morganmmoore0@gmail.com>
2021-11-04 21:24:43 -0400

    created dashboard route

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,14 +1,31 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
 
-    
+    const title = document.querySelector('#project-name').value.trim();
+    const content = document.querySelector('#project-desc').value.trim();
+  
+    if (title && content) {
+      const response = await fetch(`/api/projects`, {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    }
 };
 
 const delButtonHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
         const id = event.target.getAttribute('data-id');
 
-        const response = await fetch(`/apis/projects/${id}`, {
+        const response = await fetch(`/api/projects/${id}`, {
             method: 'DELETE',
         });
 
