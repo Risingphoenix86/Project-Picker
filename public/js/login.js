5a1be9bdba130ab5b2fc92b2675f46513060b624
morganmmoore <morganmmoore0@gmail.com>
2021-11-04 20:48:02 -0400

    fixed small typo

--- public/js/login.js
+++ public/js/login.js
@@ -27,7 +27,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-signup').value.trim();
   
     if (username && email && password) {
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/api/users/', {
         method: 'POST',
         body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
