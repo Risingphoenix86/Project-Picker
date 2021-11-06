8f78507dbef348d1d7173741a3a9443e8da203ad
Katlynn Blanton <katlynnblanton@gmail.com>
2021-11-06 13:12:04 -0400

    adding res.render for all projects

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const projectRoutes = require('./api/projectRoutes');
+const projectRoutes = require('./projectRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/api/projectRoutes', projectRoutes);
+router.use('/projects', projectRoutes);
 module.exports = router;
