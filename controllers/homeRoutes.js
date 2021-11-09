5c5ab6219f00d2f0b3e3e50f0de0b10b5d5a21f5
Kyle Lux <kyleslux86@gmail.com>
2021-11-08 21:45:54 -0500

    fixed hompage redirect to signin/signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User, Project } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req,res) => {
+router.get('/', async (req,res) => {
     try {
         const userData = await User.findAll({
             attributes: { exclude: ['password'] },
